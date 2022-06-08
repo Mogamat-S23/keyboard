b6de1cfdffe9d91efbbdcd6244767b0747ab1d01
Mogamat-S23 <saleemariefdien@gmail.com>
2022-06-08 08:30:18 +0200

    keyboard

---
+++ scripts/code.js
@@ -0,0 +1,14 @@
+// function addchar(char){
+//     document.querySelector("#output").innerHTML += char
+//   }
+//   function delchar(){
+//     document.querySelector("#output").innerHTML = document.querySelector("#output").innerHTML.slice(0, -1)
+//   }
+
+
+  function addChar(char){
+    document.querySelector("#output").innerHTML += char
+  }
+  function delChar(){
+    document.querySelector("#output").innerHTML = document.querySelector("#output").innerHTML.slice(0, -1)
+  }
